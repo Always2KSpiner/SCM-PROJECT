f4ac737fb169462fec2b4f28953f4a197451626e
Darsh <500061595@gmail.com>
2018-04-25 15:51:57 +0530

    created a function for heapifying the tree

--- project.cpp
+++ project.cpp
@@ -9,6 +9,8 @@ using namespace std;
 /*
  * Node Declaration
  */
+
+
 struct node
 {
     int info;
@@ -26,6 +28,29 @@ class BST
             root = NULL;
         }
 };
+
+// A function to heapify the bianry tree.
+void heapify(int arr[], int n, int i)
+{
+    int largest = i;  
+    int l = 2*i + 1;  
+    int r = 2*i + 2; 
+ 
+    // If left child is larger than root
+    if (l < n && arr[l] > arr[largest])
+        largest = l;
+ 
+    // If right child is larger than largest so far
+    if (r < n && arr[r] > arr[largest])
+        largest = r;
+ 
+    // If largest is not root
+    if (largest != i)
+    {
+        swap(arr[i], arr[largest]);
+        heapify(arr, n, largest);
+    }
+}
 int main()
 
 {
