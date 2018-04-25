5ae7b8d43265aa7d73db92d48b51d78bf98686a7
Always2KSpiner <500061147@stu.upes.ac.in>
2018-04-25 10:06:51 +0530

    Inorder Traversal Struct Created

--- project.cpp
+++ project.cpp
@@ -1,2 +1,14 @@
 //This is the intial project file 
 //all work will be done on this
+# include <iostream>
+# include <cstdlib>
+using namespace std;
+/*
+ * Node Declaration
+ */
+struct node
+{
+    int info;
+    struct node *left;
+    struct node *right;
+}*root;
